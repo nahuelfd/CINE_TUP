68e1600f1514ed68176998b8ce6eed60f6e2cfc3
nahuelfd <nahuelfd@gmail.com>
2025-10-20 19:00:45 -0300

    agregado de cors para conexion front-back

--- Server/Cine_Tup_Server/src/index.js
+++ Server/Cine_Tup_Server/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-
 import movieRoutes from './routes/movie.routes.js';
+import userRoutes from './routes/user.routes.js';
 import { sequelize } from "./db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 
 import './entities/Movie.js';
@@ -12,19 +13,31 @@ const app = express();
 
 try {
     app.use(express.json());
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "*");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        next();
-    })
+    app.use(cors({
+    origin: "http://localhost:5173",                // origen permitido (tu frontend Vite)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+    // credentials: true, // descomentar si vas a usar cookies/sesiones con credenciales
+  }));
+    // codigo sugerido del profe: *pero no funciona bien
+    //*app.use((req, res, next) => {
+      //  res.header("Access-Control-Allow-Origin", "*");
+      //  res.header("Access-Control-Allow-Headers", "*");
+    //    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+     //   next();
+    //}) 
 
-    app.listen(PORT);
+    
     app.use(movieRoutes);
+    app.use(userRoutes);
 
     await sequelize.sync();
 
-    console.log(`Server listening on port ${PORT}`)
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+
+    
 
 
 } catch (error) {
