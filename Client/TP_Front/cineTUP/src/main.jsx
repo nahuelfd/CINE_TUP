9e469c689d58bf98589d4c5e09db34c650f9ce41
franvelayos <fvelayos01@gmail.com>
2025-10-24 02:20:05 -0300

    Agregado de Contexto Tema Claro/Oscuro

--- Client/TP_Front/cineTUP/src/main.jsx
+++ Client/TP_Front/cineTUP/src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './App.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
