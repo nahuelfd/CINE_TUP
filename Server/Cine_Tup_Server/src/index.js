83e2dc47d5545c39021c2e0c272f10f16a4a9095
franvelayos <fvelayos01@gmail.com>
2025-10-26 14:07:16 -0300

    Funcionalidad compra tickets user (Server)

--- Server/Cine_Tup_Server/src/index.js
+++ Server/Cine_Tup_Server/src/index.js
@@ -1,41 +1,36 @@
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "../.env") }); // apunta a cine_tup_server/.env
-
-import { config } from "./config.js"; // ahora sí tomará los valores de env
 import express from "express";
 import { sequelize } from "./db.js";
+import { config } from "./config.js";
+
 import movieRoutes from "./routes/movie.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import ticketRoutes from "./routes/ticket.routes.js";
-
+import cors from "cors";
 import "./entities/Movie.js";
 import "./entities/User.js";
 import "./entities/Ticket.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-import "./entities/Movie.js";
-dotenv.config();
-const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
+app.use(cors({
+  origin: "http://localhost:5173", // el origen de tu frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
-app.use(movieRoutes);
-app.use(userRoutes);
-app.use(ticketRoutes);
+
+app.use("/api", movieRoutes);
+app.use("/api", userRoutes);
+app.use("/api", ticketRoutes);
 
 try {
   await sequelize.sync();
@@ -45,3 +40,4 @@ try {
 } catch (error) {
   console.error("Error initializing server:", error);
 }
+
